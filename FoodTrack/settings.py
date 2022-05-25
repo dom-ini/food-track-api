badc23310b5c1de9e2ad85ee7d00404ce96ad4bf
Dominik <dominik.szlaza@gazeta.pl>
2022-05-25 17:00:59 +0200

    Added STATIC_ROOT to settings

--- FoodTrack/settings.py
+++ FoodTrack/settings.py
@@ -131,6 +131,7 @@ LOCALE_PATHS = (BASE_DIR / 'locale', )
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
