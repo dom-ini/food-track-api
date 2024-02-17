c9ebd4c4d08a781466bb908c27145ef8764f82ec
Dominik <dominik.szlaza.1@gmail.com>
2024-02-17 11:32:52 +0100

    refactor: move verification products admin action to class

--- products/admin.py
+++ products/admin.py
@@ -3,14 +3,13 @@ from django.contrib import admin
 from products.models import Product
 
 
-@admin.action(description="Zweryfikuj wybrane produkty")
-def make_verified(_modeladmin, _request, queryset):
-    queryset.update(is_verified=True)
-
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name", "is_verified"]
-    actions = [make_verified]
+    actions = ["make_verified"]
+
+    @admin.action(description="Zweryfikuj wybrane produkty")
+    def make_verified(self, _request, queryset):
+        queryset.update(is_verified=True)
 
 
 admin.site.register(Product, ProductAdmin)
